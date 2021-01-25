f001097f25869729e374368a95ad581a3dd3bcb5
issakuss <issakuss@gmail.com>
2021-01-25 13:42:51 +0900

    fix: Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-mkdir -p ~/dotfiles
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config ~/.config
 ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
