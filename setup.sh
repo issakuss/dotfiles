6d6cc267ce65d868c7c2d8cf2c784f9824b6eb72
issakuss <issakuss@gmail.com>
2022-07-16 10:27:50 +0900

    🔬 feat: Add code snippets for markdown

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config ~/.config
 ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/.vscode/snippets ~/Library/Application\ Support/Code/User/snippets
