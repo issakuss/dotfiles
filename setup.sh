c05ba54039c9d8a21cf603a6e4bbbe07b762ba95
issakuss <issakuss@gmail.com>
2024-03-08 09:15:02 +0900

    🔬 feat: Stop to manage vscode settings

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.config ~/.config
-ln -s ~/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/.vscode/snippets ~/Library/Application\ Support/Code/User/
